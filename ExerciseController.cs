432ba2986aac5ac30dc48eed28a126a3a8023a0b
bigvvill <wcurtisusa@gmail.com>
2022-12-08 07:47:54 -0500

    Building Db

--- ExerciseController.cs
+++ ExerciseController.cs
@@ -1,10 +1,24 @@
-﻿namespace ExerciseTracker
+﻿using bigvvill.CodeReviews.Console.ExerciseTracker.Models;
+
+namespace ExerciseTracker
 {
     internal class ExerciseController
     {
         internal void AddSession()
         {
-            Console.WriteLine("Hi");
+
+
+            Console.WriteLine("dropping into DB\nint Id *\nDateTime DateStart 2022-12-8 08:00:00\nDateTime DateEnd 2022-12-8 08:40:00\nTimeSpan Duration *\nstring Comments Eliptical");
+
+            Exercise exercise = new Exercise();
+            exercise.DateStart = DateTime.Parse("2022-12-8 08:00:00");
+            exercise.DateEnd = DateTime.Parse("2022-12-8 08:40:00");
+            exercise.Comments = "Eliptical";
+
+            List<Exercise> exercises= new List<Exercise>();
+            exercises.Add(exercise);
+
+            
         }
 
         internal void GetSessions()
